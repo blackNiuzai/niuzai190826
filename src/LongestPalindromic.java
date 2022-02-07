f4e7c0fccb8dfd73bd42a8b603de2773af9143ac
yuanjing.lin <yuanjing.lin@shopee.com>
2022-02-07 20:17:02 +0800

    day1.1

--- src/LongestPalindromic.java
+++ src/LongestPalindromic.java
@@ -1,16 +1,39 @@
 public class LongestPalindromic {
 
-
-    public String solution(String s){
-
-
-
-        return null;
+    public static void main(String[] args) {
+        LongestPalindromic test = new LongestPalindromic();
+        String str = test.solution("babad");
+        System.out.println(str);
     }
 
 
+    public String solution(String s){
+        if ((null == s) || s.length()==0){return "";}
+        String longest = "";
+        int len = s.length();
+        for (int i = 0; i < len; i++) {
+            String l1 = getLongestStr(s, i, i);
+            longest = l1.length()>longest.length()? l1: longest;
+            String l2 = getLongestStr(s, i, i + 1);
+            longest = l2.length()>longest.length()? l2: longest;
+        }
 
 
+        return longest;
+    }
+
+    private String getLongestStr(String s, int i, int j) {
+        if(j > s.length() - 1){return "";}
+        while( i >= 0 && j <= s.length() - 1){
+            if (s.charAt(i)==s.charAt(j)){
+                i--;
+                j++;
+            }else {
+                break;
+            }
+        }
+        return s.substring( i+1, j);
+    }
 
 
 }
